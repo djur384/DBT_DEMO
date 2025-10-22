d7e86d2ae8a7ab4cf6d7900d97b402edbf91a205
shakhnozatilyaeva@gmail.com <shakhnozatilyaeva@gmail.com>
2025-10-22 00:31:06 +0000

    correction of the data to mutch 2018 datas

--- models/station_dimension.sql
+++ models/station_dimension.sql
@@ -2,15 +2,15 @@ WITH BIKE as (
 
 select
 distinct
-start_statio_id AS station_id,
+start_station_id AS station_id,
 start_station_name AS station_name,
 start_lat AS station_lat,
 start_lng AS start_station_lng
 
 
-from {{ source('demo', 'bike') }}
+from {{ ref('stg_bike') }}
 
-where RIDE_ID !='ride_id'
+where RIDE_ID != '"bikeid"'
 
 
 )
